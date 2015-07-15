b4d14731e84e44b0dff524ea6bb0475584b7d87e
Samuel San <samuelsaninbox@gmail.com>
2015-07-15 23:52:40 +0000

    ok

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-	validates :email, presence: true format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: {case_sensitive: false}
+	validates :email, presence: true #format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: {case_sensitive: false}
 	validates :password, presence: true
 	validates :usertype, presence: true
 
