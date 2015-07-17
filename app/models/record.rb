d5b10593a33f8abece973ef194c9efd93bffc01e
Tom Lei Mac Vagrant <mengda.lei@mail.mcgill.ca>
2015-07-17 19:11:36 +0000

    Reord.add now will create a new row in Record table for each tenant each month due on the 27th of the current month

--- app/models/record.rb
+++ app/models/record.rb
@@ -7,9 +7,19 @@ class Record < ActiveRecord::Base
   validates :location_id, presence: true
   validates :date_due, presence: true
 
-def add(address, tenant_id, landlord_id, amount_paid, amount_due, date_due)
-  Record.create(created_at: DateTime.now, address: address, tenant_id: tenant_id, landlord_id: landlord_id, amount_paid: amount_paid, amount_due: amount_due, date_due: date_due)
-  self.save
+def self.add()
+		today = Date.today
+		duedate = Date.new(today.year, today.month,27)
+	User.where.not(location_id:nil).each do |user|
+		location = Location.find(user.location_id)
+		landlord = User.find(location.landlord_id)
+		Record.create(tenant_id:user.id,landlord_id:landlord.id,location_id:location.id,amount_due:location.rate,amount_paid:0,date_due:duedate,date_paid:nil)
+	end
 end
+
+#def add(address, tenant_id, landlord_id, amount_paid, amount_due, date_due)
+#  Record.create(created_at: DateTime.now, address: address, tenant_id: tenant_id, landlord_id: landlord_id, amount_paid: amount_paid, amount_due: amount_due, date_due: date_due)
+#  self.save
+#end
 
 end
