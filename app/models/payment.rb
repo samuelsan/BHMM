b37da38af975dbf4efc95bfcf60c6de618bad4e4
Tom Lei Mac Vagrant <mengda.lei@mail.mcgill.ca>
2015-07-17 18:39:29 +0000

    added model file for payments

---
+++ app/models/payment.rb
@@ -0,0 +1,9 @@
+def Payment < ActiveRecord::Base
+validates :amount, presence: true
+validates :landlord_id, presence: true
+validates :tenant_id, presence: true
+validates :location_id, presence: true
+
+belongs_to :location
+
+end
