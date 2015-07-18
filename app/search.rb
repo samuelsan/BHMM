632009f255c7989b26320e1554627d028edbe49e
Tom Lei Mac Vagrant <mengda.lei@mail.mcgill.ca>
2015-07-18 03:24:09 +0000

    search now can search by landlord's anme and display their properties

--- app/search.rb
+++ app/search.rb
@@ -2,7 +2,7 @@ def search(search_text)
 	# search function, returns an array of location objects found.
 	time = Time.now	
 	result = []
-	Location.all.each { |location| result << location if location.address.downcase.include? search_text.downcase }
+	Location.all.each { |location| result << location if location.address.downcase.include? search_text.downcase or user.find(location.landlord_id).name.include? search_text }
   puts "Search took #{(Time.now - time)*100}ms to complete"
 	result
 end
